6b79ea4d02c87bfbee80b6d2b66e7b6fa8b853de
Manav Misra <manavm@visionwebsoft.com>
2024-10-09 00:56:46 -0500

    feat: :sparkles: render animated squares

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,10 @@
+import tailwindcssMotion from "tailwindcss-motion";
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.js"],
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [tailwindcssMotion],
 };
